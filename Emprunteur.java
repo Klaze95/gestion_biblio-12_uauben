e2cbf8036e6d9e2b7257ee32c9e88d6cd7902b00
DAO <d.fairousse@gmail.com>
2026-06-24 10:50:01 +0000

    Ajout de la classe Emprunteur

---
+++ Emprunteur.java
@@ -0,0 +1,31 @@
+
+public class Emprunteur {
+    private String nom;
+    private String prenom;
+    private String numeroAdherant;
+
+    public Emprunteur(String nom, String prenom, String numeroAdherant) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.numeroAdherant = numeroAdherant;
+    }
+
+    // Getters 
+    public String getNom() {
+        return nom;
+    }
+    public String getPrenom() {
+        return prenom;
+    }
+    public String getNumeroAdherant() {
+        return numeroAdherant;
+    }
+    //Annotation
+    @Override
+    public String toString() {
+        return "Emprunteur{" +
+                "nom='" + nom + "', prenom='" + prenom + "', numeroAdherant='" + numeroAdherant + "'}";
+    }
+}
+   
+
