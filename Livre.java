ae0f115f27d83473030871b9e8fc915aae07f708
DAO <d.fairousse@gmail.com>
2026-06-25 10:25:32 +0000

    Gestion penalité de retard dans un emprunt

--- Livre.java
+++ Livre.java
@@ -4,6 +4,7 @@ public class Livre {
     private String auteur;
     private String isbn;
     private boolean disponible;
+    private double penalite = 0; // La pénalité est initialisée à 0 par défaut
 // Rendre public mes attributs
     public Livre(String titre, String auteur, String isbn) {
         this.titre = titre;
@@ -27,6 +28,24 @@ public class Livre {
     }
     public void setDisponible(boolean disponible) {
         this.disponible = disponible;
+    }
+    // Emprunter un livre
+    public void emprunter() {
+        if (disponible) {
+            if (penalite > 0) {
+                System.out.println("Attention: penalité de retard de "+ penalite + "Francs CFA à regler avant l'emprunt");
+                return;
+            }
+            disponible = false;
+            System.out.println("Le livre a été emprunté avec succès.");
+        }
+        else {
+            System.out.println("Le livre n'est pas disponible pour l'emprunt.");
+        }
+    }
+    public void appliquerPenaliteRetard(int joursDeRetard){
+        double tauxParJour = 500; // F CFA par jour de ratard
+        this.penalite += joursDeRetard * tauxParJour;
     }
     //Annotation
     @Override
